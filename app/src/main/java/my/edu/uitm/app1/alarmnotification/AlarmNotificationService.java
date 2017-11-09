3b42a0ec6257a7932888226a8b42eb06090953b4
theraimi <reymous29@gmail.com>
2017-11-09 11:35:05 +0800

    Initial commit

---
+++ app/src/main/java/my/edu/uitm/app1/alarmnotification/AlarmNotificationService.java
@@ -0,0 +1,44 @@
+package my.edu.uitm.app1.alarmnotification;
+
+/**
+ * Created by student9 on 11/9/17.
+ */
+
+import android.app.IntentService;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+
+public class AlarmNotificationService extends IntentService {
+    private NotificationManager alarmNotificationManager;
+    //Notification ID for Alarm
+    public static final int NOTIFICATION_ID = 1;
+    public AlarmNotificationService() {
+        super("AlarmNotificationService");
+    }
+    @Override
+    public void onHandleIntent(Intent intent) {
+//get edittext custom message value from MainActivity
+        String notify = intent.getExtras().getString("msg");
+//Send notification
+        sendNotification(notify);
+    }
+    //handle notification
+    private void sendNotification(String msg) {
+        alarmNotificationManager = (NotificationManager) this
+                .getSystemService(Context.NOTIFICATION_SERVICE);
+//get pending intent
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
+                new Intent(this, MainActivity.class), PendingIntent.FLAG_UPDATE_CURRENT);
+//Create notification
+        NotificationCompat.Builder alamNotificationBuilder = new NotificationCompat.Builder(
+                this).setContentTitle("Alarm").setSmallIcon(R.mipmap.loceng)
+                .setStyle(new NotificationCompat.BigTextStyle().bigText(msg))
+                .setContentText(msg).setAutoCancel(true);
+        alamNotificationBuilder.setContentIntent(contentIntent);
+//notiy notification manager about new notification
+        alarmNotificationManager.notify(NOTIFICATION_ID, alamNotificationBuilder.build());
+    }
+}
